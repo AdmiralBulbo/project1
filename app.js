75ba9682b86797d779b02b29ce536d10aefd151b
Timur <timur.kri4563@gmail.com>
2020-09-22 20:14:24 +0300

    Draft for routes added

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var index = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/user', userRoutes);
 
 module.exports = app;
