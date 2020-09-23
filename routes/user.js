43642da586b238425dadaef35e6e8bd45787f656
Timur <timur.kri4563@gmail.com>
2020-09-23 19:11:44 +0300

    Added rout for signup

--- routes/user.js
+++ routes/user.js
@@ -3,3 +3,15 @@ var router = express.Router();
 var passport = require('passport');
 var csrf = require('csurf');
 
+router.post('/signup', passport.authenticate('local.signup', {
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}), function (req, res, next) {
+    if(req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+    } else {
+        res.redirect('/user/profile');
+    }
+});
